a5eb3799127f26b7c7c244bd9a98e1e5f7e945fb
Blank000000 <1848961935@qq.com>
2019-05-14 18:01:34 +0800

    生成10以内的所有随机数

--- 随机数.c
+++ 随机数.c
@@ -5,13 +5,20 @@
 int main(){
   srand((int)time(0));//使用系统时间作为随机种子
   
-  int x;
-  for(x = 0; x < 10; x++){
-    //输出100以内的随机数
-    printf("%d\n", rand_X(100));
+  int temp = 1;
+  int i;
+  int x = 0;
+  int y = 10;
+  while(i <= 10){
+    //输出10个100以内的随机数
+    temp = rand() % (y - x + 1) + x;//生成[0,10]的所有随机数
+    if(temp>10||temp<0){
+      printf("错误！\n");
+      break;
+    }
+    if(i == temp){
+      printf("temp = %d\n",temp);
+      i++;
+    }
   }
-}
-
-int rand_X(int x){
-  return rand() % x;
 }
